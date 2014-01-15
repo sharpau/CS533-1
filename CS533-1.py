1a8408831cfe8e60ce9b7d128666851b0582ee97
Austin Sharp <austinsharp111@gmail.com>
2014-01-14 18:33:22 -0800

    mdp, bellman backup might be working

--- CS533-1.py
+++ CS533-1.py
@@ -3,11 +3,21 @@
 # Finite Horizon MDP Solver
 from mdp import MDP
 
-def bellman_backup():
+def bellman_backup(mdp, value_k):
     """
-    Expected value of a state = immediate reward plus max over all actions of the
-    weighted-average reward of the possible next states.
+    Given an MDP and its value function with time-to-go k, returns its value function with time-to-go k+1.
     """
+    # computing a new value function, ie list over all states
+    new_value_fn = []
+    for s in range(mdp.num_states):
+        # R(s) + max_a[sum_s'(T(s,a,s') dot Vk(s'))]
+        action_results = []
+        for a in range(mdp.num_actions):
+            action_results.append(sum([mdp.transition(s, a, s_next) * value_k[s_next] for s_next in range(mdp.num_states)]))
+        new_value_fn.append(mdp.rewards[s] + max(action_results))
+
+
+    return new_value_fn
 
 
 def plan(mdp, horizon):
@@ -16,6 +26,14 @@ def plan(mdp, horizon):
     a time horizon H (positive integer). The output should be an optimal non-stationary value
     function and non-stationary policy for the MDP and time horizon .
     """
+    # list of value functions indexed by time-to-go k
+    value_functions = []
+    # initialize at time-to-go 0 with reward of each state
+    value_functions.append(mdp.rewards)
+    for i in range(horizon + 1):
+        value_functions.append(bellman_backup(mdp, value_functions[i]))
+    # at this point value functions should be filled up to horizon
+    return value_functions
 
 
 """
@@ -24,3 +42,4 @@ Main program flow. Test plan() on two MDPs, each with two different time horizon
 
 test = MDP("simple.txt")
 test.show()
+print plan(test, 3)
--- mdp.py
+++ mdp.py
@@ -7,16 +7,27 @@ class MDP(object):
         print "Actions: " + str(self.num_actions)
         print "States: " + str(self.num_states)
         print "Rewards: " + str(self.rewards)
+        for i in range(self.num_actions):
+            print "Action " + str(i) + " transitions: "
+            print str(self.transitions[i])
+
+    def transition(self, start_state, action, end_state):
+        return self.transitions[action][end_state][start_state] # TODO should the last two params be swapped?
 
     def __init__(self, filename):
         self.name = filename
+        self.transitions = []
 
         with open(filename, "r") as in_file:
             self.num_states = int(in_file.readline().strip())
             self.num_actions = int(in_file.readline().strip())
 
             # reward: next line of file, split on space, convert to ints
-            self.rewards = [int(x) for x in in_file.readline().strip().split()]
+            self.rewards = [float(x) for x in in_file.readline().strip().split()]
 
-            #for i in range(self.num_actions):
+            for i in range(self.num_actions):
                 # read in transition probability matrix
+                matrix = []
+                for j in range(self.num_states):
+                    matrix.append([float(x) for x in in_file.readline().strip().split()])
+                self.transitions.append(matrix)
