945ff0f8abaf2b8b00cefab042fc3d76530e8b82
Austin Sharp <austinsharp111@gmail.com>
2014-01-14 20:20:58 -0800

    also outputting policy

--- CS533-1.py
+++ CS533-1.py
@@ -1,6 +1,7 @@
 # Austin Sharp
 # CS533 Winter '14 Homework 1
 # Finite Horizon MDP Solver
+import operator
 from mdp import MDP
 
 def bellman_backup(mdp, value_k):
@@ -9,15 +10,19 @@ def bellman_backup(mdp, value_k):
     """
     # computing a new value function, ie list over all states
     new_value_fn = []
+    actions = []
     for s in range(mdp.num_states):
         # R(s) + max_a[sum_s'(T(s,a,s') dot Vk(s'))]
         action_results = []
         for a in range(mdp.num_actions):
             action_results.append(sum([mdp.transition(s, a, s_next) * value_k[s_next] for s_next in range(mdp.num_states)]))
-        new_value_fn.append(mdp.rewards[s] + max(action_results))
+
+        max_index, max_value = max(enumerate(action_results), key=operator.itemgetter(1))
+        actions.append(max_index)
+        new_value_fn.append(mdp.rewards[s] + max_value)
 
 
-    return new_value_fn
+    return new_value_fn, actions
 
 
 def plan(mdp, horizon):
@@ -28,12 +33,15 @@ def plan(mdp, horizon):
     """
     # list of value functions indexed by time-to-go k
     value_functions = []
+    policy = []
     # initialize at time-to-go 0 with reward of each state
     value_functions.append(mdp.rewards)
     for i in range(horizon + 1):
-        value_functions.append(bellman_backup(mdp, value_functions[i]))
+        vals, actions = bellman_backup(mdp, value_functions[i])
+        value_functions.append(vals)
+        policy.append(actions)
     # at this point value functions should be filled up to horizon
-    return value_functions
+    return value_functions, policy
 
 
 """
